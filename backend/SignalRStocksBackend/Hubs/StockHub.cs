fb9469b9f6b017bd61cc46f68eb170719ada515a
Philipp <philipp.niemetz@gmail.com>
2022-04-19 14:50:27 +0200

    update

--- backend/SignalRStocksBackend/Hubs/StockHub.cs
+++ backend/SignalRStocksBackend/Hubs/StockHub.cs
@@ -1,9 +1,35 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRStocksBackend.Services;
 
 namespace SignalRStocksBackend.Hubs
 {
     public class StockHub : Hub
     {
-        
+        private static int clientCount = 0;
+        private readonly StockService stockService;
+
+        public StockHub(StockService stockService)
+        {
+            this.stockService = stockService;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            clientCount++;
+            Clients.All.SendAsync("ClientCountChanged", clientCount);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            clientCount--;
+            Clients.All.SendAsync("ClientCoundChanged", clientCount);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public void BuyShare(string username, string shareName, int amount, bool isBuy)
+        {
+            var transaction = stockService.
+        }
     }
 }
--- backend/SignalRStocksBackend/Services/StockService.cs
+++ backend/SignalRStocksBackend/Services/StockService.cs
@@ -12,4 +12,5 @@ public class StockService
         this.db = db;
     }
 
+
 }
